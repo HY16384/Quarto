c06a2cc67c8c27ce121ba72b6ede03f615921b30
HaYa <44742501+HY16384@users.noreply.github.com>
2022-05-01 02:14:40 +0900

    add many function

--- public/js/playground.js
+++ public/js/playground.js
@@ -1,31 +1,136 @@
 const socket = io()
 socket.on('first-player-selection-start', (board) => {
   console.log('first-player-selection-start')
-  initBoard(board)
-  
+  initBoard(board["board-state"])
+  $(".next-piece-container").children("img").remove()
+  setRemaininigs(board["remainings"],true)
+  //この下系うまくいってない
+  $("#player1").addClass("active")
+  $("#player2").removeClass("active")
+  $(".remaining-pieaces-container").addClass("clickable")
+  $(".board").removeClass("clickable")
 })
 
-function initBoard(board) {
-  const data = JSON.parse(board)
-  const boardState = data["board-state"]
-  const remainings = data["remainings"]
-  for(let i=0; i<4; i++) {
-    for(let j; j<4; j++) {
-      let position = (i+1) + "-" + (j+1)
+socket.on('second-player-put-start', (type, board) => {
+  console.log('second-player-put-start')
+  console.log(board)
+  initBoard(board["board-state"])
+  initRemaininigs(board["remainings"],false)
+  setPiece(type)
+  $("#player1").removeClass("active")
+  $("#player2").addClass("active")
+  $(".remaining-pieaces-container").removeClass("clickable")
+  $(".board").addClass("clickable")
+})
+
+socket.on('second-player-selection-start', (board) => {
+  console.log('second-player-selection-start')
+  initBoard(board["board-state"])
+  setRemaininigs(board["remainings"],true)
+  $(".next-piece-container").children("img").remove()
+  $("#player1").removeClass("active")
+  $("#player2").addClass("active")
+  $(".remaining-pieaces-container").addClass("clickable")
+  $(".board").removeClass("clickable")
+})
+
+socket.on('first-player-put-start', (type, board) => {
+  console.log('first-player-put-start')
+  setPiece(type, true)
+  initBoard(board["board-state"])
+  initRemaininigs(board["remainings"],true)
+  $("#player1").addClass("active")
+  $("#player2").removeClass("active")
+  $(".remaining-pieaces-container").removeClass("clickable")
+  $(".board").addClass("clickable")
+})
+
+function initBoard(boardState) {
+  for(let i = 0; i < 4; i++) {
+    for(let j = 0; j < 4; j++) {
+      let position = ((i+1) + "-" + (j+1)).toString()
       let state = boardState[position]
+      $("#board-"+position).children("img").remove()
       if (state != "") {
-        $("#board-"+position).children("img").attr("src", "")
-        $("#board-"+position).children("img").attr("src", "/img/"+state+".svg")
+        let img = $("<img>").attr("src", "/img/"+state+".svg")
+        $("#board-"+position).addClass("piecein")
+        $("#board-"+position).append(img.clone())
       } else {
-        $("#board-"+position).children("img").attr("src", "")
+        $("#board-"+position).children("img").remove()
       }
     }
   }
+}
 
-  for(let k = 0; k<16; k++) {
+function setRemaininigs(remainings, isFirst) {
+  for(let k = 0; k < Object.keys(remainings).length; k++) {
+    $("#remain-"+(k+1).toString()).children("img").remove()
     let piece = remainings[k]
+    console.log(remainings)
     if (piece != "") {
-      $("#remain-"+(k+1)).children("img").attr("src", "/img/"+piece+".svg")
+      let img = $("<img>").attr("src", "/img/"+piece+".svg")
+      $("#remain-"+(k+1).toString()).append(img.clone())
+      $("#remain-"+(k+1).toString()).on("click", function() {
+        for(let l = 0; l<16; l++) {
+          if(l != k) {
+            $("#remain-"+(l+1).toString()).removeClass("active")
+          }
+        }
+        $("#remain-"+(k+1).toString()).toggleClass("active")
+        $("#remain-"+(k+1).toString()).children(".remaining-pieace-btn").on("click", function() {
+          if(isFirst == true) {
+            socket.emit('first-player-select-the-piece', piece)
+          } else {
+            socket.emit('second-player-select-the-piece', piece)
+          }
+        })
+      })
+    } else {
+      $("#remain-"+(k+1).toString()).children("img").remove()
+    }
+  }
+}
+
+function initRemaininigs(remainings, isFirst) {
+  for(let k = 0; k < Object.keys(remainings).length; k++) {
+    $("#remain-"+(k+1).toString()).children("img").remove()
+    let piece = remainings[k]
+    console.log(remainings)
+    if (piece != "") {
+      let img = $("<img>").attr("src", "/img/"+piece+".svg")
+      $("#remain-"+(k+1).toString()).append(img.clone())
+
+    } else {
+      $("#remain-"+(k+1).toString()).children("img").remove()
+    }
+  }
+}
+
+function setPiece(type, isFirst) {
+  let img = $("<img>").attr("src", "/img/"+type+".svg")
+  $(".next-piece-container").append(img.clone())
+  for(let i = 0; i < 4; i++) {
+    for(let j = 0; j < 4; j++) {
+      let position = ((i+1) + "-" + (j+1)).toString()
+      $("#board-"+position).on("click", function() {
+        for(let k = 0; k < 4; k++) {
+          for(let l = 0; l < 4; l++) {
+            if(k!=i || l!=j) {
+              let other = ((k+1) + "-" + (l+1)).toString()
+              $("#board-"+other).removeClass("active")
+            }
+          }
+        }
+        $("#board-"+position).addClass("active")
+
+        $("#board-"+position).children(".board-btn").on("click", function() {
+          if (isFirst == true) {
+            socket.emit('first-player-put-the-piece', type, i+1, j+1)
+          } else {
+            socket.emit('second-player-put-the-piece', type, i+1, j+1)
+          }
+        })
+      })
     }
   }
 }
--- server.js
+++ server.js
@@ -4,53 +4,161 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+  extended: true
+}))
 app.use(express.static("public"))
-const http = require('http').Server(app);
-
-
-app.get("/", function(req, res) {
-  res.render("home")
-})
+const http = require('http').Server(app)
 
 let boardlist = {
   "board-state": {
-    "1-1": "", "1-2": "", "1-3": "", "1-4":"",
-    "2-1": "", "2-2": "", "2-3": "", "2-4":"",
-    "3-1": "", "3-2": "", "3-3": "", "3-4":"",
-    "4-1": "", "4-2": "", "4-3": "", "4-4":""
+    "1-1": "",
+    "1-2": "",
+    "1-3": "",
+    "1-4": "",
+    "2-1": "",
+    "2-2": "",
+    "2-3": "",
+    "2-4": "",
+    "3-1": "",
+    "3-2": "",
+    "3-3": "",
+    "3-4": "",
+    "4-1": "",
+    "4-2": "",
+    "4-3": "",
+    "4-4": ""
   },
   "remainings": ["0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"]
 }
+
+let boardJson = JSON.parse(JSON.stringify(boardlist))
+
+let state = 0
 
 app.get("/playground", function(req, res) {
   res.render("playground")
 })
 
+app.get("/", function(req, res) {
+  if (state != -1) {
+    res.redirect('/playground')
+  }else {
+    res.render("home")
+  }
+})
+
 const io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
-  socket.emit("first-player-selection-start",JSON.stringify(boardlist))
-
+  switch (state) {
+    case 0:
+      socket.emit("first-player-selection-start", boardJson)
+      break
+    case 1:
+      socket.emit("second-player-put-start", "", boardJson)
+      break
+    case 2:
+      socket.emit("second-player-selection-start", boardJson)
+      break
+    case 3:
+      socket.emit("first-player-put-start", "", boardJson)
+      break
+  }
   socket.on('first-player-select-the-piece', (type) => {
+    boardJson["remainings"] = boardJson["remainings"].filter(item => item != type)
+    state = 1
+    console.log(boardJson,state)
 
-    socket.emit("second-player-put-start",boardlist)
+    socket.emit("second-player-put-start", type, boardJson)
   })
-  socket.on('second-player-put-the-piece', (type, positionX, positionY) => {
 
-    socket.emit("second-player-selection-start",boardlist)
+  socket.on('second-player-put-the-piece', (type, x, y) => {
+    boardJson["board-state"][(x + "-" + y).toString()] = type
+    console.log(boardJson["board-state"][(x + "-" + y).toString()])
+    // TODO: 勝利処理
+    let isEnd = false
+    for (let i = 0; i < 4; i++) {
+      let isXOk = true
+      let isYOk = true
+      for (let j = 0; j < 4; j++) {
+        let xLine = boardJson["board-state"][((j+1) + "-" + y).toString()]
+        let yLine = boardJson["board-state"][(x + "-" + (j+1)).toString()]
+        console.log(xLine,yLine)
+        if (xLine!="" && yLine!="") {
+          if (type.charAt(i)!=xLine.charAt(i)) {
+            isXOk = false
+          }
+          if (type.charAt(i)!=yLine.charAt(i)) {
+            isYOk = false
+          }
+        } else {
+          isXOk = false
+          isYOk = false
+        }
+      }
+      if(isXOk == true || isYOk == true) {
+        isEnd = true
+      }
+    }
+    if(isEnd == false) {
+      state = 2
+      //// TODO: 2から2に抜けてる
+      console.log(boardJson,state)
+      socket.emit("first-player-selection-start", boardJson)
+    } else {
+      state = 5
+      console.log(boardJson,state)
+      console.log("player2 win")
+    }
   })
+
   socket.on('second-player-select-the-piece', (type) => {
-
-    socket.emit("first-player-put-start",boardlist)
+    boardJson["remainings"] = boardJson["remainings"].filter(item => item != type)
+    state = 3
+    console.log(boardJson,state)
+    socket.emit("first-player-put-start", type, boardJson)
   })
-  socket.on('first-player-put-the-piece', (type, positionX, positionY) => {
 
-    socket.emit("first-player-selection-start",boardlist)
+  socket.on('first-player-put-the-piece', (type, x, y) => {
+    boardJson["board-state"][(x + "-" + y).toString()] = type
+    // TODO: 勝利処理
+    let isEnd = false
+    for (let i = 0; i < 4; i++) {
+      let isXOk = true
+      let isYOk = true
+      for (let j = 0; j < 4; j++) {
+        let xLine = boardJson["board-state"][((j+1) + "-" + y).toString()]
+        let yLine = boardJson["board-state"][(x + "-" + (j+1)).toString()]
+        console.log(xLine,yLine)
+        if (xLine=="") {
+          isXOk = false
+        } else if (type.charAt(i)!=xLine.charAt(i)) {
+          isXOk = false
+        }
+        if (xLine=="") {
+          isXOk = false
+        } else if (type.charAt(i)!=yLine.charAt(i)) {
+          isYOk = false
+        }
+      }
+      if(isXOk == true || isYOk == true) {
+        isEnd = true
+      }
+    }
+    if(isEnd == false) {
+      state = 0
+      console.log(boardJson,state)
+      socket.emit("second-player-selection-start", boardJson)
+    } else {
+      state = 4
+      console.log(boardJson,state)
+      console.log("player1 win")
+    }
   })
 
 })
 
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log("Server started");
 })
